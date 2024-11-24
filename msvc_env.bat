b4b9a95413dd7db8bd27ce8aafeb641b7abd8b66
김동건 <104804087+gbdngb12@users.noreply.github.com>
2024-11-24 15:47:37 +0900

    Update msvc_env.bat

--- msvc_env.bat
+++ msvc_env.bat
@@ -8,3 +8,10 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 #!/bin/bash
 vmrun -T ws start /home/dong/vmware/lsw/lsw.vmx nogui
 sshpass -p dong ssh dong@192.168.159.130
+
+
+
+bcdedit /debug on
+bcdedit /dbgsettings net hostip:192.168.139.129 port:50000
+
+debugger에서는 Key값 넣어주기
