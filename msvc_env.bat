00e533e3e92ee0cab8648d94af265f40de9f4673
김동건 <104804087+gbdngb12@users.noreply.github.com>
2024-11-24 15:32:56 +0900

    Update msvc_env.bat

--- msvc_env.bat
+++ msvc_env.bat
@@ -3,3 +3,8 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+
+
+#!/bin/bash
+vmrun -T ws start /home/dong/vmware/lsw/lsw.vmx nogui
+sshpass -p dong ssh dong@192.168.159.130
